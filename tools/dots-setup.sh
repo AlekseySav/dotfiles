5d4208a5034a70a19300f05d0fee7ebd6ff02240
schetvkomplax <schet.v.komplax@gmail.com>
2024-03-18 19:57:58 +0300

    add coretools

--- tools/dots-setup.sh
+++ tools/dots-setup.sh
@@ -1,21 +1,5 @@
 # install dotfiles
 
-# install core utils
-# brew install coreutils
-# brew install fzf
-# brew install jq
-# brew install ripgrep
-# brew install tree
-
-# install dev tools
-# brew install neovim
-# brew install tmux
-# brew install zoxide
-
-# install languages
-# brew install rust
-# brew install zig
-
 # fetch repo
 cd ~
 if [ ! -d .dotfiles ]; then
---
+++ tools/install-coretools.sh
@@ -0,0 +1,15 @@
+# install core utils
+brew install coreutils
+brew install fzf
+brew install jq
+brew install ripgrep
+brew install tree
+
+# install dev tools
+brew install neovim
+brew install tmux
+brew install zoxide
+
+# install languages
+brew install rust
+brew install zig
