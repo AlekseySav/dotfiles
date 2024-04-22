355cd48f91e8b745202a3908112eb6ad83c262fa
AlekseySav <alex2003s@yandex.ru>
2024-04-22 03:03:10 +0300

    simplify

--- README.md
+++ README.md
@@ -1,29 +1,26 @@
 ## my dotfiles
 
-### primary shell & package managers:
-- `bash` &mdash; default shell
-- `homebrew` &mdash; package manager for CLI tools
-- `mason.nvim` &mdash; package manager for lsp & dap
-- `lazy.nvim` &mdash; package manager for nvim plugins
-- `tpm` &mdash; package manager for tmux
+- `tmux` with `tpm` for plugins
+- `nvim` with `lazy` for plugins and `mason` for lsp and dap
+
+### setup
+- clone repo: `git clone git@github.com:AlekseySav/dotfiles.git ~/.dotfiles`
+- add bash config (`$HOME/.dotfiles/config/bash/profile.sh`) to profile
+- run setup: `dots setup`
+
+### prerequisites
+
+- `nvim`, `ripgrep`, `fzf`, `tmux`, `zoxide` have to be installed
+- if `brew` is found, it will be used to install them, so it is prefered
+- only `bash` is supported
 
 ### code organization:
-- `.data/` &mdash; for backups, cache, etc.
 - `config/` &mdash; each config is linked to `~/.config/$name`
 - `tools/` &mdash; small scripts, are accessed by `dots tool` command
-- `dots` &mdash; dotfiles manager, is linked to `/usr/local/bin/dots`
+- `dots.sh` &mdash; dotfiles manager, is linked to `/usr/local/bin/dots`
 
 ### dots
 - `dots backup` &mdash; copy all managed dotfiles to `~/.dotfiles/.data/backup/`
 - `dots install` &mdash; link all managed dotfiles to this repo
 - `dots update` &mdash; fetch configs from remote repository
-- `dots info` &mdash; list all configs, all intsalled packages, etc.
-- `dots save-info` &mdash; save info to `~/.dotfiles/info.yaml`
 - `dots tool <cmd> [args...]` &mdash; run tool from `~/.dotfiles/tools/`
-
-### setup
-- install brew: `/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`
-- clone dots: `git clone git@github.com:AlekseySav/dotfiles.git ~/.dotfiles`
-- add bash config .profile: `source ~/.dotfiles/config/bash/profile.sh`
-- setup dots: `dots setup`
-- install core tools: `dots tool install-coretools`
--- tools/dots-setup.sh
+++ tools/dots-setup.sh
@@ -1,14 +1,51 @@
 # install dotfiles
 
-# fetch repo
-cd ~
-if [ ! -d .dotfiles ]; then
-	git clone git@github.com:AlekseySav/dotfiles.git .dotfiles
+cd $HOME/.dotfiles
+
+rm -rf config/tmux/plugins/tpm
+rm -rf .data/sources .data/bin
+
+mkdir -p .data/configs .data/cache .data/backups .data/sources .data/bin/dir
+
+ln -s dots.sh .data/bin/dots
+
+# install dependencies
+
+git clone https://github.com/tmux-plugins/tpm.git config/tmux/plugins/tpm
+
+if command -v brew &> /dev/null; then
+	brew install zoxide fzf ripgrep neovim tmux
+	ln -s $(brew --prefix)/rg .data/bin/rg
+	exit
 fi
 
-cd .dotfiles
-git pull
+echo -n >.data/configs/setup_config
+echo "https://github.com/ajeetdsouza/zoxide/releases: " >>.data/configs/setup_config
+echo "https://github.com/junegunn/fzf/releases: " >>.data/configs/setup_config
+echo "https://github.com/BurntSushi/ripgrep/releases: " >>.data/configs/setup_config
+echo "https://github.com/tmux/tmux/releases: " >>.data/configs/setup_config
+echo "https://github.com/neovim/neovim/releases: " >>.data/configs/setup_config
+vim .data/configs/setup_config
 
-# install dots
-# rm -f /usr/local/bin/dots
-# ln -s "$HOME/.dotfiles/dots" /usr/local/bin/dots
+while read line; do
+	name=$(echo $line | cut -d' ' -f1 | rev | cut -d'/' -f2 | rev)
+	link=$(echo $line | cut -d' ' -f2)
+	echo $name $link
+	if [[ $(echo $line | cut -d' ' -f1) == $link ]]; then
+		echo "bad link" >&2
+		exit 1
+	fi
+	rm -rf .data/bin/dir/$name
+	mkdir .data/bin/dir/$name
+	curl -sLJO $line >.data/sources/$name.tar.gz
+	tar xfz .data/sources/$name.tar.gz -C .data/bin/dir/$name
+	if [[ $name == "tmux" ]]; then
+		(cd .data/bin/dir/tmux/*; ./configure --enable-utf8proc >/dev/null && make --silent)
+	fi
+done <<<"$(cat .data/configs/setup_config)"
+
+ln -s .data/bin/zoxide_dir/zoxide .data/bin/zoxide
+ln -s .data/bin/dir/fzf/fzf .data/bin/fzf
+ln -s .data/bin/dir/ripgrep/*/rg .data/bin/rg
+ln -s .data/bin/dir/neovim/*/bin/nvim .data/bin/nvim
+ln -s .data/bin/dir/tmux/*/tmux .data/bin/tmux
