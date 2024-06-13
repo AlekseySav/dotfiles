0ab2d7ad2512cf96cf8f8804c8454a19b99a635a
schetvkomplax <schet.v.komplax@gmail.com>
2024-06-13 18:05:32 +0300

    update nvim mason

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -63,7 +63,7 @@ require('lazy').setup {
 			{ 'williamboman/mason.nvim', opts = {} },
 			{
 				'williamboman/mason-lspconfig.nvim',
-				opts = { automatic_installation = true },
+				opts = { --[[ automatic_installation = true --]] },
 			},
 		},
 	},
