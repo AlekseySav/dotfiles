1df14a0874c834915c9176489cdc3c6a9eb602b8
schet <schet.v.komplax@gmail.com>
2024-10-29 17:43:19 +0300

    add ssh login for win

--- hooks/win/wsl.bat
+++ hooks/win/wsl.bat
@@ -2,6 +2,13 @@
 set /p "distro=wsl distro: "
 if !%distro%! == !u24!	set distro=Ubuntu-24.04
 if !%distro%! == !u18!	set distro=Ubuntu-18.04
+if !%distro%! == !ssh!	goto command
 @echo Running WSL with %distro%
 
 @C:\\Windows\\System32\\wsl.exe ~ -d %distro% -e ${zsh.path}
+@exit
+
+: command
+set /p "user=username: "
+set /p "port=port: "
+@ssh -p %port% %user%@127.0.0.1 -t ${zsh.path}
