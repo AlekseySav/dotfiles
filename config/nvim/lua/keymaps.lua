7e62d5803b7ddb93141823e76999c88a54db35a3
schet <schet.v.komplax@gmail.com>
2024-11-27 22:46:07 +0300

    fix zinit

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -87,7 +87,9 @@ vim.keymap.set('n', '<leader>gs', require('gosource').change_extension({
 	{ '.pb.h',  '.proto' },
 	{ '.pb.cc', '.proto' },
 	{ '.h',     '.cpp',  create = true },
+	{ '.hpp',   '.cpp',  create = true },
 	{ '.cpp',   '.h' },
+	{ '.cpp',   '.hpp' },
 	{ '.h',     '.c', },
 	{ '.c',     '.h' },
 }))
