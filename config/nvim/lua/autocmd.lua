27299aa08603369e543215953b0a4a5a19166522
alexey.savinov <alexey.savinov@tel-core.ru>
2025-03-05 19:24:23 +0300

    add auto-update of saved files

--- config/nvim/lua/autocmd.lua
+++ config/nvim/lua/autocmd.lua
@@ -9,3 +9,19 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
 		end
 	end
 })
+
+local config_path = os.getenv('HOME') .. '/.dotfiles/config'
+local copy_to_config_path = os.getenv('HOME') .. '/.dotfiles/hooks/copy_to_config'
+local Job = require('plenary.job')
+
+vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+	callback = function()
+		local file = vim.fn.expand('%:p')
+		if #file > #config_path and file:sub(1, #config_path) == config_path then
+			Job:new({
+				command = copy_to_config_path,
+				args = { file:sub(#config_path + 2) },
+			}):start()
+		end
+	end
+})
